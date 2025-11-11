be27e45a20308df12b2d80993aedb4774b73043f
JGM01 <jacobgermanamccray@gmail.com>
2025-11-11 18:41:25 -0500

    better benchmarks

--- benches/document_chunking.rs
+++ benches/document_chunking.rs
@@ -2,11 +2,41 @@ use criterion::{Criterion, criterion_group, criterion_main};
 use std::path::Path;
 use wubrag::*;
 
-fn bench_chunk_documents(c: &mut Criterion) {
-    let root_path = Path::new(".");
+fn bench_chunk_coreutils(c: &mut Criterion) {
+    let root_path = Path::new("coreutils");
     let docs = grab_all_documents(std::hint::black_box(&root_path));
 
-    c.bench_function("chunk_all_documents", |b| {
+    c.bench_function("chunk_coreutils", |b| {
+        b.iter(|| {
+            let _ = chunk_all_documents(std::hint::black_box(&docs));
+        })
+    });
+}
+fn bench_chunk_ladybird(c: &mut Criterion) {
+    let root_path = Path::new("ladybird");
+    let docs = grab_all_documents(std::hint::black_box(&root_path));
+
+    c.bench_function("chunk_ladybird", |b| {
+        b.iter(|| {
+            let _ = chunk_all_documents(std::hint::black_box(&docs));
+        })
+    });
+}
+fn bench_chunk_dolphin(c: &mut Criterion) {
+    let root_path = Path::new("dolphin");
+    let docs = grab_all_documents(std::hint::black_box(&root_path));
+
+    c.bench_function("chunk_dolphin", |b| {
+        b.iter(|| {
+            let _ = chunk_all_documents(std::hint::black_box(&docs));
+        })
+    });
+}
+fn bench_chunk_ratatui(c: &mut Criterion) {
+    let root_path = Path::new("ratatui");
+    let docs = grab_all_documents(std::hint::black_box(&root_path));
+
+    c.bench_function("chunk_ratatui", |b| {
         b.iter(|| {
             let _ = chunk_all_documents(std::hint::black_box(&docs));
         })
@@ -16,7 +46,7 @@ fn bench_chunk_documents(c: &mut Criterion) {
 criterion_group! {
     name = doc_benches;
     config = Criterion::default().sample_size(10);
-    targets = bench_chunk_documents
+    targets = bench_chunk_ratatui, bench_chunk_dolphin, bench_chunk_ladybird, bench_chunk_coreutils
 }
 
 criterion_main!(doc_benches);
